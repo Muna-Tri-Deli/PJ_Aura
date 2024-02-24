9cddfba0f9f3d0fc536a3af387218957794e6da9
Warlock9527 <xiaochen9991@outlook.com>
2024-02-24 17:02:53 -0500

    init ability actor info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -18,6 +18,11 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 
 public:
 	AAuraEnemy();
+
+	/** Enemy Interface */
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	/** End Enemy Interface */
+
+	virtual void BeginPlay() override;
 };
