af23e54c9eb5124b2e6189910ec5d1a1b3d6b7b9
Warlock9527 <xiaochen9991@outlook.com>
2024-02-23 21:37:06 -0500

    Derived class for character

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright MUNA152
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Copyright MUNA152
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
